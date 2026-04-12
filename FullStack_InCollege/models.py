4ee1d3c2b130afc895c08cd2cf158b1c05bf2f0b
shivam_raj <shivamraj81020051@gmail.com>
2026-04-13 04:41:47 +0530

    django file removed

--- FullStack_InCollege/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    topic = models.CharField(max_length=200)
-    language = models.CharField(max_length=100)
-    duration = models.IntegerField()
-
-    def __str__(self):
-        return self.topic
--- FullStack_InCollege/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import project_view
-
-urlpatterns = [
-    path('', project_view, name='project'),
-]
