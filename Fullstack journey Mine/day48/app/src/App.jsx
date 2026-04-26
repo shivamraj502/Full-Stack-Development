a47b4cb52968be229ac76f9a15b309e4a6513a2c
shivam_raj <shivamraj81020051@gmail.com>
2026-04-26 21:23:10 +0530

    custom hooks

--- Fullstack journey Mine/day48/app/src/App.jsx
+++ Fullstack journey Mine/day48/app/src/App.jsx
@@ -92,7 +92,6 @@ function App() {
       ) : (
         data.map(user => (
           <p key={user.id}>{user.name}</p>
-          // <p key={user.id}>{user.name}</p>
         ))
       )}
     </div>
