703e629222198721c5befca5e41c09b1e42b2877
shivam_raj <shivamraj81020051@gmail.com>
2026-04-28 23:53:56 +0530

    first commit for performance code

--- Fullstack journey Mine/day50/app/src/App.jsx
+++ Fullstack journey Mine/day50/app/src/App.jsx
@@ -6,3 +6,22 @@ Optimize component re-render
  */
 
 
+import React, { useState } from "react";
+
+const Child = React.memo(({ name }) => {
+  console.log("Child Rendered");
+  return <h2>{name}</h2>;
+});
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Child name="Shivam" />
+      <button onClick={() => setCount(count + 1)}>Click</button>
+    </div>
+  );
+}
+
+export default App;
