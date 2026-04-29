ffa4a240d6842db837af7bdf1b8b7ef16d571e51
shivam_raj <shivamraj81020051@gmail.com>
2026-04-29 14:24:45 +0530

    installation and basic tailwind

---
+++ Fullstack journey Mine/day52/app/src/App.jsx
@@ -0,0 +1,19 @@
+/**
+Week 8 (Days 52–58): Tailwind + Deployment
+Day 52 – Tailwind Basics
+Task:
+Install Tailwind
+👉 Build:
+Styled button, card
+ */
+
+
+function App(){
+  return (
+    <>
+    {/* <h1>hello dear brother and sister</h1> */}
+    <h1 className="text-3xl text-blue-500 bg-blue ">Tailwind Working ✅</h1>
+
+    </>
+  );
+}export default App;
