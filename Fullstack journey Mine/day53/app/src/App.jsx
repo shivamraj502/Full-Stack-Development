8b537b10bf312cfc27c6c90ecfb0dd1bbad36d6d
shivam_raj <shivamraj81020051@gmail.com>
2026-04-30 12:43:38 +0530

    bug solving 3rd day

---
+++ Fullstack journey Mine/day53/app/src/App.jsx
@@ -0,0 +1,60 @@
+function App(){
+  return (
+    <>
+    {/* <h1>hello dear brother and sister</h1> */}
+    <h1 className="text-3xl text-blue-500 bg-blue ">Tailwind Working ✅</h1>
+
+    </>
+  );
+}export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import './App.css'
+
+// function App() {
+
+//   const [user, SetUser] = useState("Shivam");
+
+//   return (
+//     <div className='bg-black text-white h-screen w-screen flex flex-col items-center justify-center text-3xl font-bold'>
+//       <h1>{user}</h1>
+//       <button className='bg-white text-black m-4 w-60 rounded-3xl' onClick={() => SetUser("John")}>
+//         Click
+//       </button>
+//       <button className='bg-white text-black m-4 w-60 rounded-3xl' onDoubleClick={() => SetUser("Shivam")}>
+//         DoubleCLick
+//       </button>
+//     </div>
+//   )
+// }
+
+// export default App
