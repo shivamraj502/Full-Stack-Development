892ea46613a1e8a44f37d81f48567ee82b9eb40b
shivam_raj <shivamraj81020051@gmail.com>
2026-06-23 16:14:54 +0530

    Authentication

--- Fullstack journey Mine/day108/server.js
+++ Fullstack journey Mine/day108/server.js
@@ -12,6 +12,9 @@ Notes App / Task Manager
 
 const express = require("express");
 const connectDB = require("./config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
 
 const app = express();
 
@@ -21,6 +24,53 @@ connectDB();
 
 app.get("/", (req, res) => {
     res.send("Task Manager Backend Running");
+});
+
+app.post("/register", async (req, res) => {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        email,
+        password: hashedPassword
+    });
+
+    await user.save();
+
+    res.json({
+        message: "User Registered"
+    });
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(401).json({
+            message: "Invalid password"
+        });
+    }
+
+    const token = jwt.sign(
+        { id: user._id },
+        "secretkey"
+    );
+
+    res.json({
+        message: "Login successful",
+        token
+    });
 });
 
 app.listen(3000, () => {
