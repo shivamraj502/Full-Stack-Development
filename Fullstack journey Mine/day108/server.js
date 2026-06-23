877d433cb0c282ed712edf849d5a797fe87fe9e3
shivam_raj <shivamraj81020051@gmail.com>
2026-06-23 15:58:10 +0530

    Project Planning: decide topic

---
+++ Fullstack journey Mine/day108/server.js
@@ -0,0 +1,11 @@
+/**
+⚡ Week 16 (Days 108–114): Full CRUD + Deployment
+Day 108 – Project Planning
+
+Task:
+Decide project structure
+👉 Example:
+Notes App / Task Manager
+ */
+
+                // Task Manager
