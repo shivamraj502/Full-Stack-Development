5a57f3949d70e74a6a16610435c4eee32147aaa0
shivam_raj <shivamraj81020051@gmail.com>
2026-06-24 15:52:25 +0530

    CI/CD Basics

---
+++ Fullstack journey Mine/day118/server.js
@@ -0,0 +1,31 @@
+/**
+Day 118 – CI/CD Basics
+
+Topic: Automation
+Task:
+Learn GitHub Actions basics
+👉 Do:
+Simple workflow (build/test)
+ */
+
+/**
+What is CI/CD?
+
+CI = Continuous Integration
+CD = Continuous Deployment / Delivery
+
+
+
+Meaning
+CI
+Automatically:
+build code
+test code
+check errors
+after every push.
+
+CD
+Automatically deploy app.
+Example:
+Push code → Test → Deploy
+ */
