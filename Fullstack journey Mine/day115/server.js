4b67ace7aa4fc6bf6167453b033f523c90603a89
shivam_raj <shivamraj81020051@gmail.com>
2026-06-24 15:12:52 +0530

    git

---
+++ Fullstack journey Mine/day115/server.js
@@ -0,0 +1,21 @@
+/**
+📅 Month 5: Advanced Topics + Deployment
+⚡ Week 17 (Days 115–121): Git & Deployment
+Day 115 – Git Basics
+
+Topic: init, add, commit, push
+Task:
+Install Git
+Create repo
+👉 Do:
+Push your previous project
+ */
+
+/*
+git init
+git remote add origin "url"
+git branch -M main
+git add .
+git commit -m "comment"
+git push or git push origin main
+*/
