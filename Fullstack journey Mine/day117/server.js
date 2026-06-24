a646b1053ba81970da20dc3c4e3fef759f5fdf53
shivam_raj <shivamraj81020051@gmail.com>
2026-06-24 15:46:21 +0530

    Pull request

---
+++ Fullstack journey Mine/day117/server.js
@@ -0,0 +1,35 @@
+/**
+Day 117 – GitHub PR
+
+Topic: Pull requests
+Task:
+Create PR
+👉 Do:
+Review & merge
+ */
+
+/**git pull origin main/master/feature
+
+Your command is valid, but it is not for creating PR.
+git pull origin main
+Meaning:
+Download latest code from GitHub main branch to local system
+
+git branch feature2
+git checkout feature2 or git checkout -b feature2
+git add .
+git commit -m ""
+git push origin feature
+git checkout main
+
+Step 5: Review & Merge
+Click on GitHub:
+Create Pull Request
+Review
+Merge
+
+Step 6: Pull updated main
+After merge:
+git checkout main
+git pull origin main/master/feature
+ * */
