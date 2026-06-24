8e2da7c7d23763c31dc4ea557bde5378fe8bcc23
shivam_raj <shivamraj81020051@gmail.com>
2026-06-24 13:02:55 +0530

    Multiple paths

--- Fullstack journey Mine/day108/server.js
+++ Fullstack journey Mine/day108/server.js
@@ -10,6 +10,7 @@ Notes App / Task Manager
 
                 // Task Manager
 
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const bcrypt = require("bcrypt");
@@ -75,6 +76,8 @@ app.post("/login", async (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
